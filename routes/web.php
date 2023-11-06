6f1bdab0ca893ff96d74b0d5269a724b418230f6
yosepb <yosepbahtiar400@gmail.com>
2023-11-06 08:43:23 +0700

    Perubahan Video 3 WPU

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Yosep Bahtiar",
+        "email" => "yosepbahtiar400@gmail.com",
+        "image" => "yosep.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
