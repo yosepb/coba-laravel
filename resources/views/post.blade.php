4188553088f72e550af183d8c2d992b2a0c5d12e
yosepb <yosepbahtiar400@gmail.com>
2023-11-08 09:19:36 +0700

    Perubahan Video 8 WPU

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
+        <p>by. Bahtiar in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {{-- <h5>by {{ $post['author'] }}</h5> --}}
         {{-- {{ $post->body }} --}}
         {!! $post->body !!}
